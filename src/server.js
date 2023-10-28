8292f2ab6414583328cc8255cd4b4b7e03c6d2f6
Giovany Lawall Inocêncio <ino.giovany@gmail.com>
2023-10-28 14:29:37 -0300

    Query Params

--- src/server.js
+++ src/server.js
@@ -12,5 +12,12 @@ app.get("/message/:id/:user", (request, response) => {
   `)
 })
 
+app.get("/users", (request, response) => {
+  //http://localhost:3333/users?page=2&limit=15
+  const {page, limit} = request.query
+
+  response.send(`Page: ${page}. Show: ${limit}`)
+})
+
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
