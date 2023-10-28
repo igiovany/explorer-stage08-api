8e514153db676dd9fb237d0c4b74d54eeda7d8b6
Giovany Lawall Inocêncio <ino.giovany@gmail.com>
2023-10-28 15:00:23 -0300

    Post method - Body Params

--- src/server.js
+++ src/server.js
@@ -1,23 +1,15 @@
 const express = require("express")
 
 const app = express()
+app.use(express.json())
 
-app.get("/message/:id/:user", (request, response) => {
+app.post("/users", (request, response) => {
+  const { name, email, password } = request.body
 
-  const { id, user } = request.params
-
-  response.send(`
-    Message ID: ${id}. 
-    For user: ${user}.
-  `)
+  response.json({ name, email, password })
 })
 
-app.get("/users", (request, response) => {
-  //http://localhost:3333/users?page=2&limit=15
-  const {page, limit} = request.query
 
-  response.send(`Page: ${page}. Show: ${limit}`)
-})
 
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
