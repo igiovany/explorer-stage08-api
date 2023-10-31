eda1dbcfc28a47cef2921bfee09ea38ea22ccdeb
Giovany Lawall Inocêncio <ino.giovany@gmail.com>
2023-10-30 21:04:03 -0300

    List tags on notes index and only tags

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -37,8 +37,8 @@ class NotesController {
     const { id } = request.params
 
     const note = await knex("notes").where({ id }).first()
-    const tags = await knex("tags").where({ note_id: id}).orderBy("name")
-    const links = await knex("links").where({ note_id: id}).orderBy("created_at")
+    const tags = await knex("tags").where({ note_id: id }).orderBy("name")
+    const links = await knex("links").where({ note_id: id }).orderBy("created_at")
 
     return response.json({
       ...note,
@@ -56,12 +56,42 @@ class NotesController {
   }
 
   async index(request, response) {
-    const { user_id } = request.query
-    const notes = await knex("notes")
-    .where({ user_id })
-    .orderBy("title")
+    const { title, user_id, tags } = request.query
 
-    return response.json(notes)
+    let notes
+
+    if (tags) {
+      const filterTags = tags.split(',').map(tag => tag.trim())
+
+      notes = await knex("tags")
+      .select([
+        "notes.id",
+        "notes.title",
+        "notes.user_id"
+      ])
+      .where("notes.user_id", user_id)
+      .whereLike("notes.title", `%${title}`)
+      .whereIn("name", filterTags)
+      .innerJoin("notes", "notes.id", "tags.note_id")
+      .orderBy("notes.title")
+    } else {
+      notes = await knex("notes")
+        .where({ user_id })
+        .whereLike("title", `%${title}`)
+        .orderBy("title")
+    }
+
+    const userTags = await knex("tags").where({ user_id })
+    const notesWithTags = notes.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id)
+
+      return {
+        ...note,
+        tags: noteTags
+      }
+    })
+
+    return response.json(notesWithTags)
   }
 }
 
